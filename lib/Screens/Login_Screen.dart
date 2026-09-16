c40f3094d9a1d778a381e4d8eb7f274cfc0ab38a
Alison Villavicencio <alison.villavicencio24903530@estu.unan.edu.ni>
2026-09-16 00:47:47 -0600

    Inicio del Login

---
+++ lib/Screens/Login_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login_Screen extends StatelessWidget {
+  const Login_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: const Color(0xFF0F1B3D),
+      body: Container(
+       
+      ), // body Container
+    );
+  }
+}
