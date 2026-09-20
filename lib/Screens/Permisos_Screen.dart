2eafa9b10458499a3f6cbc98c8a1091096265b05
Alison Villavicencio <alison.villavicencio24903530@estu.unan.edu.ni>
2026-09-20 15:47:12 -0600

    Avances de la pantalla permisos

---
+++ lib/Screens/Permisos_Screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '/Widgets/menu_Widget.dart';
+import '/Widgets/TopBar_Widget.dart';
+
+//class Permisos_Screen extends StatefulWidget {
+  //const Permisos_Screen({super.key});
+ 
+ // @override
+ // State<Permisos_Screen> createState() => _PermisosScreenState();
+//}
+
+//class _PermisosScreenState extends State<Permisos_Screen> 
+//{}
